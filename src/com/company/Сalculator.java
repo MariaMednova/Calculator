fddcfe59df5849f46b19673f5839f0cf3153710a
MariaMednova <maeria.guseynova@gmail.com>
2021-08-19 15:54:01 +0300

    ДЗ 7 2

--- src/com/company/Сalculator.java
+++ src/com/company/Сalculator.java
@@ -1,33 +1,33 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Сalculator  {
+    private static Object Exception;
 
+    public static void main(String[] args) {
+        calculator();
+    }
     public static void calculator() {
         System.out.println("введите aрифмитическое выражение в формате - 2 плюс 3:  ");
         Scanner sc = new Scanner(System.in);
         String expression = sc.nextLine();
         String[] expressionArray = expression.split(" ");
-        int first=0;
-        int second=0;
+        double first=0;
+        double second=0;
         try {
-            first = Integer.parseInt(expressionArray[1]);
-            second = Integer.parseInt(expressionArray[2]);
+            first = Double.parseDouble(expressionArray[0]);
+            second = Double.parseDouble(expressionArray[2]);
         } catch (NumberFormatException number) {
             System.out.println("Неккоректный формат числа введите еще раз");
             number.printStackTrace();
+            return;
         }
-        try {
-            MathOperation.valueOf(expressionArray[2]);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Введена некорректаная математическая операция, попробуйте еще раз");
-            e.printStackTrace();
-        }
-        printResultMathOperation(expressionArray[2],first,second);
+        printResultMathOperation(expressionArray[1],first,second);
     }
-    public static void printResultMathOperation(String s, int arg1, int arg2) {
-        int result = 0;
+    public static void printResultMathOperation(String s, double arg1, double arg2) {
+        double result = 0;
         if (s.equalsIgnoreCase(MathOperation.PLUS.mathNameRussian)) {
             result = arg1 + arg2;
         } else if (s.equalsIgnoreCase(MathOperation.MINUS.mathNameRussian))
@@ -37,15 +37,21 @@ public class Сalculator  {
         result = arg1 * arg2;
     }
     else if (s.equalsIgnoreCase(MathOperation.DIVIDE.mathNameRussian)){
-                try {
-                    result = arg1 / arg2;
-                } catch (ArithmeticException a) {
-                    a.printStackTrace();
-                    System.out.println("Деление на ноль");
+               if (arg2!=0.0){
+                    result = arg1 / arg2;}
+               else {
+                   try {
+                       throw (Throwable) Exception;
+                   } catch (Throwable throwable) {
+                       System.out.println("Деление на ноль");
+                       throwable.printStackTrace();
+                       return;
+                   }
                 }
                 }
+    else {System.out.println("Введена некорректаная математическая операция, попробуйте еще раз"); return;}
 
-       System.out.printf("Результат арифмитичесого выражение %d %s %d = %d",arg1,s,arg2,result);
+       System.out.printf("Результат арифмитичесого выражение: %f %s %f = %f",arg1,s,arg2,result);
     }
 
 
